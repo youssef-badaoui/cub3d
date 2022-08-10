cbc1b2584fcaedcb19e6b5a865b363ff1a9282a9
Youssef Badaoui <badaouiyoussef8888@gmail.com>
2022-08-10 18:42:25 +0100

    test get meta

--- Makefile
+++ Makefile
@@ -3,14 +3,14 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: Ma3ert <yait-iaz@student.42.fr>            +#+  +:+       +#+         #
+#    By: ybadaoui <ybadaoui@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/04 18:27:34 by Ma3ert            #+#    #+#              #
-#    Updated: 2022/08/04 18:32:22 by Ma3ert           ###   ########.fr        #
+#    Updated: 2022/08/10 18:34:04 by ybadaoui         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-SRC = cub3d.c utils/gnl.c
+SRC = cub3d.c 
 
 OBJ = $(SRC:.c=.o)
 
--- cub3d.c
+++ cub3d.c
@@ -6,22 +6,22 @@
 /*   By: ybadaoui <ybadaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/04 18:30:15 by Ma3ert            #+#    #+#             */
-/*   Updated: 2022/08/10 16:47:52 by ybadaoui         ###   ########.fr       */
+/*   Updated: 2022/08/10 18:38:02 by ybadaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./headers/cub3d.h"
 
-static int store(int fd, t_map *map)
-{
-	int	i;
+// static int store(int fd, t_map *map)
+// {
+// 	int	i;
 
-	i = 0;
-	while(i < map->map_h)
-	{
+// 	i = 0;
+// 	while(i < map->map_h)
+// 	{
 		
-	}
-}
+// 	}
+// }
 
 static int	get_meta_data(int fd, t_map *map)
 {
@@ -33,8 +33,8 @@ static int	get_meta_data(int fd, t_map *map)
 	while(i < 6)
 	{
 		line = gnl(fd);
-
-
+		if(!line)
+			return (0);
 		if (ft_is_meta(line))
 		{
 			from = ft_is_meta(line);
@@ -43,10 +43,13 @@ static int	get_meta_data(int fd, t_map *map)
 				from += 3;
 			else
 				from += 2;				
-			map->meta_data[meta_type] = ft_substr(line, from, NULL);
+			map->meta_data[meta_type] = ft_substr(line, from, 0);
 		}
+		else if(!ft_is_empty(line))
+			return (0);
 		i++;
 	}
+	return (1);
 }
 
 static int	inspect(int fd, t_map *map)
@@ -54,14 +57,15 @@ static int	inspect(int fd, t_map *map)
 	char *line;
 
 	get_meta_data(fd, map);
-	while(1)
-	{
-		line = gnl(fd);
-		if(!line)
-			break;
+	// while(1)
+	// {
+	// 	line = gnl(fd);
+	// 	if(!line)
+	// 		break;
 
-		map->map_h++;
-	}
+	// 	map->map_h++;
+	// }
+	return (1);
 }
 
 static int check_and_stor(int ac, char **av, t_map *map)
@@ -84,4 +88,5 @@ int main(int ac, char **av)
 	init_map(map);
 	if(!check_and_stor(ac, av, map))
 		return (0);
+	return (1);
 }
