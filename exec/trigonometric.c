d94fe413c3fab77e8be4b8a801f5dd9af0a2bb4d
Youssef Badaoui <badaouiyoussef8888@gmail.com>
2022-08-19 12:19:31 +0100

    for merge

--- exec/trigonometric.c
+++ exec/trigonometric.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   trigonometric.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Ma3ert <yait-iaz@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ybadaoui <ybadaoui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 12:00:35 by Ma3ert            #+#    #+#             */
-/*   Updated: 2022/08/19 11:19:25 by Ma3ert           ###   ########.fr       */
+/*   Updated: 2022/08/19 11:45:40 by ybadaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,103 +39,6 @@ void	create_trigonometric_tables(int narc, t_table *table)
 	table->sin_table = sin_res;
 }
 
-
-void	calcul_first_inter(t_table *table, t_ray *ray, t_position position)
-{
-	if (ray->ray_pov < 180 && ray->ray_pov >= 0)
-	{
-		ray->xbound = CELL_SIZE * position.x_cell;
-		ray->x_step = CELL_SIZE;
-	}
-	else
-	{
-		ray->xbound = CELL_SIZE * (position.x_cell - 1);
-		ray->x_step = CELL_SIZE * -1;	
-	}
-	ray->yi = ((ray->xbound - position.virtual_px) / table->tan_table[ray->index]) \
-	 + position.virtual_py;
-	if (ray->ray_pov > 270 || ray->ray_pov <= 90)
-	{
-		ray->y_step = CELL_SIZE * -1;
-		ray->ybound = (position.y_cell - 1) * CELL_SIZE;
-	}
-	else
-	{
-		ray->y_step = CELL_SIZE;
-		ray->ybound = position.y_cell * CELL_SIZE;
-	}
-	ray->xi = ((ray->ybound - position.virtual_py) * table->tan_table[ray->index]) \
-		+ position.virtual_px;
-}
-
-double	calcul_ray_pov(t_position position, int ray)
-{
-	double	ray_pov;
-	double	frif;	//first ray in fov
-
-	frif = position.pov - 30;
-	ray_pov = frif + ray * ANG_IN_D;
-	if (ray_pov >= 360)
-		ray_pov = ray_pov - 360;
-	return (ray_pov);
-	
-}
-
-int	check_cell_type(t_ray *ray, t_position position)
-{
-	int	xcell_v;
-	int	ycell_v;
-	int	xcell_h;
-	int	ycell_h;
-
-	xcell_v = ray->xbound / CELL_SIZE;
-	ycell_v = ray->yi / CELL_SIZE;
-	ycell_h = ray->ybound / CELL_SIZE;
-	ycell_h = ray->xi / CELL_SIZE;
-	if (position.map->map_tab[xcell_h][ycell_h] == '1')
-		ray->h_hit = INTERSECTION_FOUND;
-	if (position.map->map_tab[xcell_v][ycell_v] == '1')
-		ray->v_hit = INTERSECTION_FOUND;
-	if (ray->v_hit || ray->v_hit)
-		return (1);
-	return (0);
-}
-
-void	calcul_next_inter(t_table *table, t_ray *ray, t_position position)
-{
-	ray->xbound += ray->x_step;
-	ray->ybound += ray->y_step;
-}
-
-// void	send_ray(t_table *table, t_ray *ray, t_position position)
-// {
-// 	int	inter;
-
-// 	inter = 0;
-// 	while (inter != INTERSECTION_FOUND)
-// 	{
-// 		if (check_cell_type(ray, position))
-// 			calcul_distance();
-// 		calcul_next_inter(table, ray, position);
-// 	}
-// }
-
-void	casting_rays(t_table *table, t_ray *rays, t_position position)
-{
-	int	i;
-
-	i = 1;
-	while (i < N_RAY)
-	{
-		rays[i].index = i;
-		rays[i].ray_pov = calcul_ray_pov(position, i);
-		calcul_first_inter(table, &rays[i], position);
-		rays[i].h_hit = 0;
-		rays[i].v_hit = 0;
-		// send_ray(table, &rays[i], position);
-		i++;
-	}
-}
 
 void	ft_draw_map(t_data *data)
 {
